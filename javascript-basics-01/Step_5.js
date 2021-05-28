7d8e36e028ba6355cfd47830ac3e678aac857ec9
Hussein-ayoub-96@hotmail.com <Hussein-Ayoub-96@hotmail.com>
2021-05-28 23:34:48 +0300

    javascript-basics-01 step-5 Completed

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function multiply(){
+    var number_1 = document.getElementById("first_number").value;
+    var number_2 = document.getElementById("second_number").value;
+    var theProduct = parseInt(number_1) * parseInt(number_2);  document.write(theProduct);
+          
+}
