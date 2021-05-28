d9779ab8bb568cd831fbd4469f85e3666454143f
Hussein-ayoub-96@hotmail.com <Hussein-Ayoub-96@hotmail.com>
2021-05-29 00:02:32 +0300

    javascript-basics-01 step-6 Completed

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,7 @@
+function divide(){
+number_1 = document.getElementById("first_number").value;
+number_2 = document.getElementById("second_number").value;
+
+product = parseInt(number_1) / parseInt(number_2);
+document.write(product);
+}
