9c4c6002f266374ff9dba909f9a6277e65a38f8a
Hussein-ayoub-96@hotmail.com <Hussein-Ayoub-96@hotmail.com>
2021-05-28 21:27:27 +0300

    javascript-basics-01 step-4 Completed

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+
+var Name = prompt("Your name:");
+var surname = prompt("Your surname:");
+var city = prompt("Your city:");
+
+alert("Name: " + Name + "\n" + "Surname: " + surname + "\n" + "City: " + city + "\n" );
