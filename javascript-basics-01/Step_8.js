4d560237c553e27f823b5c992f54299b45734e68
Hussein-ayoub-96@hotmail.com <Hussein-Ayoub-96@hotmail.com>
2021-05-29 19:30:42 +0300

    javascript-basics-01 step-8 Completed

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,8 @@
+function ask(){
+    ages = document.getElementById("age").value;
+    if(ages > 18){
+        document.write("you are over 18")    
+    }else{
+        document.write("you are under 18")
+    }
+}
