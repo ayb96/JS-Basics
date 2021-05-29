2d31de84f2fdf5d3d06c03254795b1f24859333c
Hussein-ayoub-96@hotmail.com <Hussein-Ayoub-96@hotmail.com>
2021-05-30 00:34:45 +0300

    Updated javascript-basics-02 step-6

---
+++ javascript-basics-02/step 6/Step_6.js
@@ -0,0 +1,72 @@
+function bigImg(){
+    const image1 = document.querySelector("#image1");
+    
+    
+
+    image1.addEventListener('mouseover', function (){
+        image1.src="images/image1_2.jpg"
+        
+    })
+    image1.addEventListener('mouseout', function (){
+        image1.src="images/image1.jpg"
+        
+    })
+
+    const image2 = document.querySelector("#image2")
+    
+
+    image2.addEventListener('mouseover', function (){
+        image2.src="images/image2_2.jpg"
+        
+    })
+    image2.addEventListener('mouseout', function (){
+        image2.src="images/image2.jpg"
+        
+    })
+
+    const image3 = document.querySelector("#image3");
+    
+    
+
+    image3.addEventListener('mouseover', function (){
+        image3.src="images/image3_2.jpg"
+        
+    })
+    image3.addEventListener('mouseout', function (){
+        image3.src="images/image3.jpg"
+        
+    })
+
+
+
+    const image4 = document.querySelector("#image4");
+    
+    
+
+    image4.addEventListener('mouseover', function (){
+        image4.src="images/image4_2.jpg"
+        
+    })
+    image4.addEventListener('mouseout', function (){
+        image4.src="images/image4.jpg"
+        
+    })
+
+
+
+    const image5 = document.querySelector("#image5");
+    
+    
+
+    image5.addEventListener('mouseover', function (){
+        image5.src="images/image5_2.jpg"
+        
+    })
+    image5.addEventListener('mouseout', function (){
+        image5.src="images/image5.jpg"
+        
+    })
+ 
+    
+  
+}
