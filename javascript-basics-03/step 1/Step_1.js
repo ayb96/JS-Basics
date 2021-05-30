79873f5913ace655f9a1ee0bfc912ee4866fc767
Hussein-ayoub-96@hotmail.com <Hussein-Ayoub-96@hotmail.com>
2021-05-31 01:31:01 +0300

    javascript-basics-03 step-1 Completed

---
+++ javascript-basics-03/step 1/Step_1.js
@@ -0,0 +1,13 @@
+
+let image1 = document.querySelector("#image1");
+    
+    
+  
+image1.addEventListener('mouseover', function (){
+        image1.style.border = "4px solid cyan";
+        
+})
+
+
+
+  
